40603278c890efef1e0d11c92a7f3522fc744ea6
Mark Eibes <mark.eibes@gmail.com>
2021-07-10 11:29:52 +0200

    More flex

--- src/Plumage/Style/Display/Flex.purs
+++ src/Plumage/Style/Display/Flex.purs
@@ -1,6 +1,5 @@
 module Plumage.Style.Display.Flex where
 
-
 import React.Basic.Emotion (Style, center, column, css, flex, flexEnd, flexStart, int, nowrap, row, spaceAround, spaceBetween, spaceEvenly, str, wrap, px)
 
 flexCol ∷ Style
@@ -17,30 +16,43 @@ flexRow =
     , flexDirection: row
     }
 
-flexGrow :: Int -> Style
+flexGrow ∷ Int → Style
 flexGrow g = css { flexGrow: int g }
 
-flexWrap :: Style
+flexWrap ∷ Style
 flexWrap = css { flexWrap: wrap }
 
-flexNoWrap :: Style
+flexNoWrap ∷ Style
 flexNoWrap = css { flexWrap: nowrap }
 
-flexWrapReverse :: Style
+flexWrapReverse ∷ Style
 flexWrapReverse = css { flexWrap: str "wrap-reverse" }
 
-gap :: Int -> Style
+gap ∷ Int → Style
 gap x = css { gap: px x }
 
-justifyStart :: Style
+justifyStart ∷ Style
 justifyStart = css { justifyContent: flexStart }
-justifyEnd :: Style
+justifyEnd ∷ Style
 justifyEnd = css { justifyContent: flexEnd }
-justifyCenter :: Style
+justifyCenter ∷ Style
 justifyCenter = css { justifyContent: center }
-justifyBetween :: Style
-justifyBetween = css { justifyContent: spaceBetween}
-justifyAround :: Style
-justifyAround = css { justifyContent: spaceAround}
-justifyEvenly :: Style
-justifyEvenly = css { justifyContent: spaceEvenly}
+justifyBetween ∷ Style
+justifyBetween = css { justifyContent: spaceBetween }
+justifyAround ∷ Style
+justifyAround = css { justifyContent: spaceAround }
+justifyEvenly ∷ Style
+justifyEvenly = css { justifyContent: spaceEvenly }
+
+itemsStart ∷ Style
+itemsStart = css { alignItems: flexStart }
+itemsEnd ∷ Style
+itemsEnd = css { alignItems: flexEnd }
+itemsCenter ∷ Style
+itemsCenter = css { alignItems: center }
+itemsBetween ∷ Style
+itemsBetween = css { alignItems: spaceBetween }
+itemsAround ∷ Style
+itemsAround = css { alignItems: spaceAround }
+itemsEvenly ∷ Style
+itemsEvenly = css { alignItems: spaceEvenly }
