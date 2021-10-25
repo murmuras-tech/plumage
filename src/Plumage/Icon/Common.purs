cd5091c667a3a1ccbcdbaf5641525e10b5630ef0
Mark Eibes <mark.eibes@gmail.com>
2021-10-25 15:28:56 +0200

    Fill none

--- src/Plumage/Icon/Common.purs
+++ src/Plumage/Icon/Common.purs
@@ -11,6 +11,7 @@ outlines ds =
     , xmlns: "http://www.w3.org/2000/svg"
     , xmlSpace: "preserve"
     , stroke: "currentColor"
+    , fill: "none"
     , children:
         ds
           <#> \d →
