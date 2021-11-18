f9180f1edc7d617c4438dd5fa0d1cef0a72dd0cf
Mark Eibes <mark.eibes@gmail.com>
2021-11-18 11:14:13 +0100

    Add more colour helpers

--- src/Plumage/Style/Color/Util.purs
+++ src/Plumage/Style/Color/Util.purs
@@ -9,6 +9,21 @@ withAlpha alpha c1 = Color.rgba' r g b alpha
   where
   { r, g, b } = Color.toRGBA' c1
 
+withLightness ∷ Number → Color → Color
+withLightness l c1 = Color.hsla h s l a
+  where
+  { h, s, a } = Color.toHSLA c1
+
+withSaturation ∷ Number → Color → Color
+withSaturation s c1 = Color.hsla h s l a
+  where
+  { h, l, a } = Color.toHSLA c1
+
+withHue ∷ Number → Color → Color
+withHue h c1 = Color.hsla h s l a
+  where
+  { s, l, a } = Color.toHSLA c1
+
 lightness ∷ Color → Number
 lightness = Color.toHSLA >>> _.l
 
