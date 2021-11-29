dbb0608de9ac744035aa3cf48c06f38f5cbbe8c3
Mark Eibes <mark.eibes@gmail.com>
2021-11-29 13:15:18 +0100

    Sun and moon icons

--- src/Plumage/Style/Opacity.purs
+++ src/Plumage/Style/Opacity.purs
@@ -1,9 +1,8 @@
 module Plumage.Style.Opacity where
 
 import Prelude
-
 import Data.Int as Int
 import React.Basic.Emotion (Style, css, num)
 
-opacity :: Int -> Style
-opacity amount = css { opacity: num ((Int.toNumber amount) / 100.0)  }
+opacity ∷ Int → Style
+opacity amount = css { opacity: num ((Int.toNumber amount) / 100.0) }
