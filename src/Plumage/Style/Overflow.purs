76c6636b288e93dffdeff142e82d4b707aaac2fb
Mark Eibes <mark.eibes@gmail.com>
2022-04-06 13:44:13 +0200

    Visible

--- src/Plumage/Style/Overflow.purs
+++ src/Plumage/Style/Overflow.purs
@@ -1,6 +1,9 @@
 module Plumage.Style.Overflow where
 
-import React.Basic.Emotion (Style, css, hidden, str)
+import React.Basic.Emotion (Style, css, hidden, str, visible)
+
+overflowVisible ∷ Style
+overflowVisible = css { overflow: visible }
 
 overflowHidden ∷ Style
 overflowHidden = css { overflow: hidden }
